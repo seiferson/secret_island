a0fc85654c8693b27900b57dd8678e06cd1850a3
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2022-12-05 21:51:43 -0600

    added base initilizr files

---
+++ src/main/java/com/seiferson/secretisland/SecretislandApplication.java
@@ -0,0 +1,13 @@
+package com.seiferson.secretisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretislandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretislandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seiferson/secretisland/SecretislandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seiferson.secretisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretislandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
