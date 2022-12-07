7b1165f5dd2d5c010a4ade72605be3381c6968a4
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2022-12-06 21:50:54 -0600

    Removed static content and added fetch from API

--- src/main/java/com/seiferson/secretisland/repository/HPostRepository.java
+++ src/main/java/com/seiferson/secretisland/repository/HPostRepository.java
@@ -1,8 +1,11 @@
 package com.seiferson.secretisland.repository;
 
 import com.seiferson.secretisland.model.HPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface HPostRepository extends MongoRepository<HPost, String> {
+
 
 }
