2583062f42348f66988833daccf6ee82d246a07b
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2022-12-08 09:23:48 -0600

    Added outh2 security using Github

---
+++ src/main/java/com/seiferson/secretisland/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.seiferson.secretisland.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .antMatchers("/")
+                    .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/hPosts")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and().oauth2Login()
+                .and().build();
+    }
+
+}
---
+++ src/main/java/com/seiferson/secretisland/controller/AppController.java
@@ -0,0 +1,17 @@
+package com.seiferson.secretisland.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/api/v1/security/userdata")
+    private Map<String, Object> getGithubUserData(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttributes();
+    }
+}
