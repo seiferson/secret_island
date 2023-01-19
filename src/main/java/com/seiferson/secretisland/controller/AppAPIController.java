e182d07f6ad842dc23f7d0258852224548f11d3e
Cuauhtemoc Herrera <seifer.ch@gmail.com>
2023-01-19 16:50:58 -0600

    Removed unused code

--- src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
+++ src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
@@ -1,8 +1,5 @@
 package com.seiferson.secretisland.controller;
 
-import com.seiferson.secretisland.model.Letter;
-import com.seiferson.secretisland.repository.LetterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +10,9 @@ import java.util.Map;
 @RestController
 public class AppAPIController {
 
-    @Autowired
-    private LetterRepository letterRepo;
-
     @GetMapping("/api/v1/security/userdata")
     private Map<String, Object> getGithubUserData(@AuthenticationPrincipal OAuth2User principal) {
         return principal.getAttributes();
     }
 
-    @GetMapping("/api/v1/emptyletter")
-    private Letter createEmptyLetter () {
-        Letter l = new Letter();
-        l.setMessage("Empty");
-        letterRepo.insert(l);
-
-
-        return l;
-    }
 }
--- src/main/java/com/seiferson/secretisland/repository/LetterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seiferson.secretisland.repository;
-
-import com.seiferson.secretisland.model.Letter;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LetterRepository extends MongoRepository<Letter, String> {
-
-}
