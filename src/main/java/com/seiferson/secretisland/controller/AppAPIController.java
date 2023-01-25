0b4d713ffa5c5422982ab03c8a40d39b6648f83c
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2023-01-24 22:13:07 -0600

    Removed Spring Data Rest, implemented own API methods

--- src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
+++ src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
@@ -3,13 +3,17 @@ package com.seiferson.secretisland.controller;
 import com.seiferson.secretisland.model.HPost;
 import com.seiferson.secretisland.repository.HPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,16 +23,21 @@ public class AppAPIController {
     private HPostRepository hPostRepo;
 
     @GetMapping("/api/v1/security/userdata")
-    private Map<String, Object> getGithubUserData(@AuthenticationPrincipal OAuth2User principal) {
+    public Map<String, Object> getGithubUserData(@AuthenticationPrincipal OAuth2User principal) {
         return principal.getAttributes();
     }
 
-    @PostMapping("api/v1/hPosts")
-    private HPost createHomePost(HPost post) {
+    @PostMapping("/api/v1/hPosts")
+    public HPost createHomePost(HPost post) {
         post.setPostedDate(new Date());
         post.setUpdated(new Date());
 
         return hPostRepo.insert(post);
+    }
+
+    @GetMapping("/api/v1/hPosts")
+    public Page<HPost> getHomePosts(Pageable pageable) {
+        return hPostRepo.findAll(pageable);
     }
 
 }
--- src/main/java/com/seiferson/secretisland/repository/HPostRepository.java
+++ src/main/java/com/seiferson/secretisland/repository/HPostRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface HPostRepository extends MongoRepository<HPost, String> {
+import java.util.List;
 
+public interface HPostRepository extends MongoRepository<HPost, String> {
+    Page<HPost> findAll(Pageable pageable);
 
 }
