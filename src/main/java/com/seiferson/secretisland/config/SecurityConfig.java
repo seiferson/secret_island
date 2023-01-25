88869caa447655785a76e3d580c3f293b0162b55
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2023-01-24 21:52:18 -0600

    Modified Home page

--- src/main/java/com/seiferson/secretisland/config/SecurityConfig.java
+++ src/main/java/com/seiferson/secretisland/config/SecurityConfig.java
@@ -17,6 +17,8 @@ public class SecurityConfig {
                     .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/hPosts")
                     .permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/hPosts")
+                    .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1")
                     .permitAll()
                 .anyRequest()
--- src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
+++ src/main/java/com/seiferson/secretisland/controller/AppAPIController.java
@@ -1,18 +1,34 @@
 package com.seiferson.secretisland.controller;
 
+import com.seiferson.secretisland.model.HPost;
+import com.seiferson.secretisland.repository.HPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.Map;
 
 @RestController
 public class AppAPIController {
 
+    @Autowired
+    private HPostRepository hPostRepo;
+
     @GetMapping("/api/v1/security/userdata")
     private Map<String, Object> getGithubUserData(@AuthenticationPrincipal OAuth2User principal) {
         return principal.getAttributes();
+    }
+
+    @PostMapping("api/v1/hPosts")
+    private HPost createHomePost(HPost post) {
+        post.setPostedDate(new Date());
+        post.setUpdated(new Date());
+
+        return hPostRepo.insert(post);
     }
 
 }
